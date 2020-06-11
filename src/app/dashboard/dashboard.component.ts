fd73c4170246cdf427095ff98ccd39678d707b7c
shinesamuel90 <shinesamuel90@gmail.com>
2020-06-11 11:52:02 +0400

    second commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,38 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class DashboardComponent implements OnInit {
   @ViewChild('sidenav',{static:true}) sidenav: MatSidenav;
   opened = true;
+  public appPages = [
+    {
+      title: 'Articles',
+      url: '/dashboard/tabs/view-articles',
+      icon: 'book'
+    },
+    {
+      title: 'Committee Members',
+      url: '/dashboard/tabs/committee-members',
+      icon: 'people'
+
+    },
+    {
+      title: 'Members',
+      url: '/dashboard/tabs/members',
+      icon: 'people-circle'
+
+    },
+    {
+      title:'gallery',
+      url:'/dashboard/tabs/gallery',
+      icon:'image'
+    },
+    {
+      title:'profile',
+      url:'/dashboard/tabs/profile',
+      icon:'person'
+    }
+
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
